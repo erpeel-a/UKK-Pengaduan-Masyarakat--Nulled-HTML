5c2cb7af1e353a1bd26f4b2958146a569bb3797f
Muhamad Zainal Arifin <zainalarifin080718@gmail.com>
2021-03-27 18:28:08 +0700

    Refactoring Menu Panel Admin

--- view/admin/index.php
+++ view/admin/index.php
@@ -7,16 +7,11 @@ Dashboard
 <!-- menu yang akan ditampilkan jika admin yang login -->
 <?php if($_SESSION['level'] === 'admin') {?>
   <a href="registrasi.php">Registrasi User</a>
-  <a href="pengaduan.php">Data Pengaduan</a>
-  <a href="Masyarakat.php">Data Masyarakat</a>
-  <a href="petugas.php">Data petugas</a>
   <a href="laporan.php">Cetak laporan </a>
-<?php }else{ ?>
-<!-- menu yang akan ditampilkan jika petugas yang login -->
+<?php }?>
   <a href="petugas.php">Data Petugas</a>
   <a href="Masyarakat.php">Data Masyarakat</a>
   <a href="pengaduan.php">Data Pengaduan</a>
-<?php } ?>
 <a href="../logout.php">Logout</a>
 
 <?php require('../layouts/footer.php') ?>
