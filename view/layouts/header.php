d9bd8dcccb6b418a277ea0ef108ca68d3acd50d1
Zainal21 <Zainalarifin080718@gmail.com>
2021-03-27 11:21:33 +0700

    Initial commit

---
+++ view/layouts/header.php
@@ -0,0 +1,18 @@
+
+
+<?php  $site_url = '' ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <title>Pengaduan Masyarakat</title>
+    <link href="<?= $site_url ?>assets/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+  <div class="container">
+    <a class="navbar-brand" href="#">Pengaduan Masyarakat</a>
+  </div>
+</nav>
